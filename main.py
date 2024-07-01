46ae36ed55db5b25985a55c9f8612155b1365b52
Bunyod <razzoqovbunyod33@gmail.com>
2024-07-01 17:12:13 +0500

    select query in python

---
+++ main.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+conn = psycopg2.connect(database='n48', user='postgres', password='1234', host='localhost', port=5432)
+
+
+my_cursor = conn.cursor()
+
+select_query = """
+select * from category;
+"""
+my_cursor.execute(select_query)
+rows = my_cursor.fetchall()
+
+for row in rows:
+    print(row)
